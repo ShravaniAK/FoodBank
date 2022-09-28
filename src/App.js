d746dd0cc8a9bdd54ae5f73e356b86777dc4407e
shravanikaware <shravanikaware12@gmail.com>
2022-09-28 16:30:36 +0530

    frontend1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Donate from './components/Donate';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+<div className="home">
+  <Home/>
+</div>
+     <div className="App">
+      
+      <Donate />
     </div>
+    </>
+   
   );
 }
 
